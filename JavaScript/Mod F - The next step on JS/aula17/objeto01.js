a2c53bd7f17866e95dff50dcdceefcc8442c3834
Guilherme Branco <144622117+GuilhermeDuarteB@users.noreply.github.com>
2024-08-13 23:11:33 +0100

    upload

---
+++ JavaScript/Mod F - The next step on JS/aula17/objeto01.js
@@ -0,0 +1,9 @@
+let amigo = {nome: `José`, 
+    sexo: `M`, 
+    peso: 85.4, 
+    console.log("engordou")
+    this.peso += p
+}}
+
+amigo.engordar(2)
+console.log(`O ${amigo.nome} tem ${amigo.peso}  `)
