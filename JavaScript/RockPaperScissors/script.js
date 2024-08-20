61ed8fe7bb564c71fc68257a01658b8969356131
Guilherme Branco <144622117+GuilhermeDuarteB@users.noreply.github.com>
2024-08-20 17:54:42 +0100

    upload

--- JavaScript/RockPaperScissors/script.js
+++ JavaScript/RockPaperScissors/script.js
@@ -3,50 +3,90 @@ let papelImg = document.getElementById("papel");
 let tesouraImg = document.getElementById("tesoura");
 let resultadoMsg = document.getElementById("mensagem");
 let imagem = document.getElementById("img");
+let contadorC = document.getElementById("contadorCPont");
+let contadorP = document.getElementById("contadorPPont");
+let resultadoC = 0; // Contador para o computador
+let resultadoP = 0; // Contador para o jogador
 
 // 0 = Pedra, 1 = Papel, 2 = Tesoura
 
-function pedraF() {
+function jogar(escolhaUsuario) {
     let randomNumber = Math.floor(Math.random() * 3);
 
+
+    if (escolhaUsuario === "pedra") {
+        pedraF(randomNumber);
+    } else if (escolhaUsuario === "papel") {
+        papelF(randomNumber);
+    } else if (escolhaUsuario === "tesoura") {
+        tesouraF(randomNumber);
+    }
+}
+
+function atualizarContadores() {
+    contadorC.innerHTML = `${resultadoC}`;
+    contadorP.innerHTML = `${resultadoP}`;
+}
+
+
+function pedraF(randomNumber) {
     if (randomNumber == 0) { // Empate
         imagem.setAttribute("src", "imgs/pedra.png");
         resultadoMsg.innerHTML = "O Computador Jogou Pedra <br> Empate";
     } else if (randomNumber == 1) { // Computador vence com Papel
+        resultadoC++
         imagem.setAttribute("src", "imgs/papel.png");
         resultadoMsg.innerHTML = "O Computador Jogou Papel <br> O Computador Venceu";
-    } else { // Você vence com Pedra
+    } else { //  vence com Pedra
+        resultadoP++
         imagem.setAttribute("src", "imgs/tesoura.png");
         resultadoMsg.innerHTML = "O Computador Jogou Tesoura <br> Venceste";
     }
+    atualizarContadores()
 }
 
-function papelF() {
-    let randomNumber = Math.floor(Math.random() * 3);
-
-    if (randomNumber == 0) { // Você vence com Papel
+function papelF(randomNumber) {
+    if (randomNumber == 0) { // vence com Papel
+        resultadoP++
         imagem.setAttribute("src", "imgs/pedra.png");
         resultadoMsg.innerHTML = "O Computador Jogou Pedra <br> Venceste";
     } else if (randomNumber == 1) { // Empate
         imagem.setAttribute("src", "imgs/papel.png");
         resultadoMsg.innerHTML = "O Computador Jogou Papel <br> Empate";
     } else { // Computador vence com Tesoura
+        resultadoC++
         imagem.setAttribute("src", "imgs/tesoura.png");
         resultadoMsg.innerHTML = "O Computador Jogou Tesoura <br> O Computador Venceu";
     }
+    atualizarContadores()
 }
 
-function tesouraF() {
-    let randomNumber = Math.floor(Math.random() * 3);
-
+function tesouraF(randomNumber) {
     if (randomNumber == 0) { // Computador vence com Pedra
+        resultadoC++
         imagem.setAttribute("src", "imgs/pedra.png");
         resultadoMsg.innerHTML = "O Computador Jogou Pedra <br> O Computador Venceu";
-    } else if (randomNumber == 1) { // Você vence com Tesoura
+    } else if (randomNumber == 1) { //  vence com Tesoura
+        resultadoP++
         imagem.setAttribute("src", "imgs/papel.png");
         resultadoMsg.innerHTML = "O Computador Jogou Papel <br> Venceste";
     } else { // Empate
         imagem.setAttribute("src", "imgs/tesoura.png");
         resultadoMsg.innerHTML = "O Computador Jogou Tesoura <br> Empate";
     }
+    atualizarContadores()
 }
+
+
+// event listeners para as imagens
+pedraImg.addEventListener("click", function() {
+    jogar("pedra");
+});
+
+papelImg.addEventListener("click", function() {
+    jogar("papel");
+});
+
+tesouraImg.addEventListener("click", function() {
+    jogar("tesoura");
+});
